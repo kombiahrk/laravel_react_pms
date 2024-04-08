b3d24bbde78fcb2d30ca84473c1b1189ecad0ea3
Kombiah <kombiahrk@yahoo.com>
2024-04-08 13:03:30 +0530

    Update an Index.jsx file within the js\Pages\Project directory to add PROJECT_STATUS_CLASS_MAP constant

--- resources/js/Pages/Project/Index.jsx
+++ resources/js/Pages/Project/Index.jsx
@@ -1,6 +1,6 @@
 import Pagination from "@/Components/Pagination";
 import AuthenticatedLayout from "@/Layouts/AuthenticatedLayout";
-import { PROJECT_STATUS_TEXT_MAP } from "@/constants";
+import { PROJECT_STATUS_CLASS_MAP, PROJECT_STATUS_TEXT_MAP } from "@/constants";
 import { Head, Link } from "@inertiajs/react";
 
 export default function Index({ auth, projects }) {
@@ -35,7 +35,9 @@ export default function Index({ auth, projects }) {
                                             <td className="px-3 py-2"><img src={project.image_path} style={{ width: 60 }} alt="image" /></td>
                                             <td className="px-3 py-2">{project.name}</td>
                                             <td className="px-3 py-2">
-                                                {PROJECT_STATUS_TEXT_MAP[project.status]}
+                                                <span className={"px-2 py-1 rounded text-white "+ PROJECT_STATUS_CLASS_MAP[project.status]}>
+                                                    {PROJECT_STATUS_TEXT_MAP[project.status]}
+                                                </span>
                                             </td>
                                             <td className="px-3 py-2 text-nowrap">{project.created_at}</td>
                                             <td className="px-3 py-2 text-nowrap">{project.due_date}</td>
