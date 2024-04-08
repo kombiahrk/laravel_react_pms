b2d977856b93e7ebe4fe5c238b46eaa8ef9b2fea
Kombiah <kombiahrk@yahoo.com>
2024-04-08 22:59:03 +0530

    Update an Index.jsx file within the js\Pages\Project directory by adding TextInput, SelectInput and define the searchFeildChanged

--- resources/js/Pages/Project/Index.jsx
+++ resources/js/Pages/Project/Index.jsx
@@ -1,9 +1,28 @@
 import Pagination from "@/Components/Pagination";
+import SelectInput from "@/Components/SelectInput";
+import TextInput from "@/Components/TextInput";
 import AuthenticatedLayout from "@/Layouts/AuthenticatedLayout";
 import { PROJECT_STATUS_CLASS_MAP, PROJECT_STATUS_TEXT_MAP } from "@/constants";
-import { Head, Link } from "@inertiajs/react";
+import { Head, Link, router } from "@inertiajs/react";
 
-export default function Index({ auth, projects }) {
+export default function Index({ auth, projects, queryParams = null }) {
+    queryParams = queryParams || {}
+    const searchFeildChanged = (name, value) => {
+        if (value) {
+            queryParams[name] = value
+        }
+        else {
+            delete queryParams[name]
+        }
+        router.get(route('project.index'), queryParams);
+    };
+
+    const onKeyPress = (name, e) => {
+        if (e.key !== 'Enter') return;
+
+        searchFeildChanged(name, e.target.value);
+    };
+
     return (
         <AuthenticatedLayout
             user={auth.user}
@@ -28,6 +47,37 @@ export default function Index({ auth, projects }) {
                                         <th className="px-3 py-3 text-right">Actions</th>
                                     </tr>
                                 </thead>
+                                <thead className="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400 border-b-2 border-gray-500">
+                                    <tr className="text-nowrap">
+                                        <th className="px-3 py-3"></th>
+                                        <th className="px-3 py-3"></th>
+                                        <th className="px-3 py-3">
+                                            <TextInput
+                                                defaultValue={queryParams.name}
+                                                className="w-full"
+                                                placeholder="Project Name"
+                                                onBlur={(e) => searchFeildChanged('name', e.target.value)}
+                                                onKeyPress={(e) => onKeyPress('name', e)}
+                                            />
+                                        </th>
+                                        <th className="px-3 py-3">
+                                            <SelectInput
+                                                defaultValue={queryParams.status}
+                                                className="w-full"
+                                                onChange={(e) => searchFeildChanged('status', e.target.value)}
+                                            >
+                                                <option value="">Select Status</option>
+                                                <option value="pending">Pending</option>
+                                                <option value="in_progress">In Progress</option>
+                                                <option value="completed">Completed</option>
+                                            </SelectInput>
+                                        </th>
+                                        <th className="px-3 py-3"></th>
+                                        <th className="px-3 py-3"></th>
+                                        <th className="px-3 py-3"></th>
+                                        <th className="px-3 py-3"></th>
+                                    </tr>
+                                </thead>
                                 <tbody>
                                     {projects.data.map(project => (
                                         <tr className="bg-white border-b dark:bg-gray-800 dark:border-gray-700" key={project.id}>
@@ -35,7 +85,7 @@ export default function Index({ auth, projects }) {
                                             <td className="px-3 py-2"><img src={project.image_path} style={{ width: 60 }} alt="image" /></td>
                                             <td className="px-3 py-2">{project.name}</td>
                                             <td className="px-3 py-2">
-                                                <span className={"px-2 py-1 rounded text-white "+ PROJECT_STATUS_CLASS_MAP[project.status]}>
+                                                <span className={"px-2 py-1 rounded text-white " + PROJECT_STATUS_CLASS_MAP[project.status]}>
                                                     {PROJECT_STATUS_TEXT_MAP[project.status]}
                                                 </span>
                                             </td>
@@ -56,7 +106,7 @@ export default function Index({ auth, projects }) {
                                     ))}
                                 </tbody>
                             </table>
-                            <Pagination links={projects.meta.links}/>
+                            <Pagination links={projects.meta.links} />
                         </div>
                     </div>
                 </div>
