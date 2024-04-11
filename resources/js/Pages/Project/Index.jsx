2ae67b8e28b8e8de49b869b2e962b8609a2b38e9
Kombiah <kombiahrk@yahoo.com>
2024-04-11 14:43:41 +0530

    Update an Index.jsx file within the js\Pages\Project directory to implement TableHeading Component

--- resources/js/Pages/Project/Index.jsx
+++ resources/js/Pages/Project/Index.jsx
@@ -4,7 +4,7 @@ import TextInput from "@/Components/TextInput";
 import AuthenticatedLayout from "@/Layouts/AuthenticatedLayout";
 import { PROJECT_STATUS_CLASS_MAP, PROJECT_STATUS_TEXT_MAP } from "@/constants";
 import { Head, Link, router } from "@inertiajs/react";
-import { ChevronUpIcon, ChevronDownIcon } from "@heroicons/react/16/solid";
+import TableHeading from "@/Components/TableHeading";
 
 export default function Index({ auth, projects, queryParams = null }) {
     queryParams = queryParams || {}
@@ -53,93 +53,47 @@ export default function Index({ auth, projects, queryParams = null }) {
                                 <table className="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400">
                                     <thead className="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400 border-b-2 border-gray-500">
                                         <tr className="text-nowrap">
-                                            <th onClick={(e) => sortChanged('id')}>
-                                                <div className="px-3 py-3 flex items-center justify-between gap-1 cursor-pointer">
-                                                    ID
-                                                    <div>
-                                                        <ChevronUpIcon className={
-                                                            "w-4 " +
-                                                            (queryParams.sort_feild === 'id' && queryParams.sort_direction === 'asc' ? 'text-white' : '')
-                                                        }
-                                                        />
-                                                        <ChevronDownIcon className={
-                                                            "w-4 -mt-2 " +
-                                                            (queryParams.sort_feild === 'id' && queryParams.sort_direction === 'desc' ? 'text-white' : '')
-                                                        }
-                                                        />
-                                                    </div>
-                                                </div>
-
-                                            </th>
+                                            <TableHeading
+                                                name="id"
+                                                sort_feild={queryParams.sort_feild}
+                                                sort_direction={queryParams.sort_direction}
+                                                sortChanged={sortChanged}
+                                            >
+                                                ID
+                                            </TableHeading>
                                             <th className="px-3 py-3">Image</th>
-                                            <th onClick={(e) => sortChanged('name')}>
-                                                <div className="px-3 py-3 flex items-center justify-between gap-1 cursor-pointer">
-                                                    Name
-                                                    <div>
-                                                        <ChevronUpIcon className={
-                                                            "w-4 " +
-                                                            (queryParams.sort_feild === 'name' && queryParams.sort_direction === 'asc' ? 'text-white' : '')
-                                                        }
-                                                        />
-                                                        <ChevronDownIcon className={
-                                                            "w-4 -mt-2 " +
-                                                            (queryParams.sort_feild === 'name' && queryParams.sort_direction === 'desc' ? 'text-white' : '')
-                                                        }
-                                                        />
-                                                    </div>
-                                                </div>
-                                            </th>
-                                            <th onClick={(e) => sortChanged('status')}>
-                                                <div className="px-3 py-3 flex items-center justify-between gap-1 cursor-pointer">
-                                                    Status
-                                                    <div>
-                                                        <ChevronUpIcon className={
-                                                            "w-4 " +
-                                                            (queryParams.sort_feild === 'status' && queryParams.sort_direction === 'asc' ? 'text-white' : '')
-                                                        }
-                                                        />
-                                                        <ChevronDownIcon className={
-                                                            "w-4 -mt-2 " +
-                                                            (queryParams.sort_feild === 'status' && queryParams.sort_direction === 'desc' ? 'text-white' : '')
-                                                        }
-                                                        />
-                                                    </div>
-                                                </div>
-                                            </th>
-                                            <th onClick={(e) => sortChanged('created_at')}>
-                                                <div className="px-3 py-3 flex items-center justify-between gap-1 cursor-pointer">
-                                                    Created Date
-                                                    <div>
-                                                        <ChevronUpIcon className={
-                                                            "w-4 " +
-                                                            (queryParams.sort_feild === 'created_at' && queryParams.sort_direction === 'asc' ? 'text-white' : '')
-                                                        }
-                                                        />
-                                                        <ChevronDownIcon className={
-                                                            "w-4 -mt-2 " +
-                                                            (queryParams.sort_feild === 'created_at' && queryParams.sort_direction === 'desc' ? 'text-white' : '')
-                                                        }
-                                                        />
-                                                    </div>
-                                                </div>
-                                            </th>
-                                            <th onClick={(e) => sortChanged('due_date')}>
-                                                <div className="px-3 py-3 flex items-center justify-between gap-1 cursor-pointer">
-                                                    Due Date
-                                                    <div>
-                                                        <ChevronUpIcon className={
-                                                            "w-4 " +
-                                                            (queryParams.sort_feild === 'due_date' && queryParams.sort_direction === 'asc' ? 'text-white' : '')
-                                                        }
-                                                        />
-                                                        <ChevronDownIcon className={
-                                                            "w-4 -mt-2 " +
-                                                            (queryParams.sort_feild === 'due_date' && queryParams.sort_direction === 'desc' ? 'text-white' : '')
-                                                        }
-                                                        />
-                                                    </div>
-                                                </div>
-                                            </th>
+                                            <TableHeading
+                                                name="name"
+                                                sort_feild={queryParams.sort_feild}
+                                                sort_direction={queryParams.sort_direction}
+                                                sortChanged={sortChanged}
+                                            >
+                                                Name
+                                            </TableHeading>
+                                            <TableHeading
+                                                name="status"
+                                                sort_feild={queryParams.sort_feild}
+                                                sort_direction={queryParams.sort_direction}
+                                                sortChanged={sortChanged}
+                                            >
+                                                Status
+                                            </TableHeading>
+                                            <TableHeading
+                                                name="created_at"
+                                                sort_feild={queryParams.sort_feild}
+                                                sort_direction={queryParams.sort_direction}
+                                                sortChanged={sortChanged}
+                                            >
+                                                Created Date
+                                            </TableHeading>
+                                            <TableHeading
+                                                name="due_date"
+                                                sort_feild={queryParams.sort_feild}
+                                                sort_direction={queryParams.sort_direction}
+                                                sortChanged={sortChanged}
+                                            >
+                                                Due Date
+                                            </TableHeading>
                                             <th className="px-3 py-3">Created By</th>
                                             <th className="px-3 py-3 text-right">Actions</th>
                                         </tr>
