e097efb11e41157609f747037c01933b3a2cd0a0
Kombiah <kombiahrk@yahoo.com>
2024-04-11 16:20:15 +0530

    Update an Index.jsx file within the js\Pages\Project directory added link tag for name to, go to the route project.show

--- resources/js/Pages/Project/Index.jsx
+++ resources/js/Pages/Project/Index.jsx
@@ -134,7 +134,11 @@ export default function Index({ auth, projects, queryParams = null }) {
                                             <tr className="bg-white border-b dark:bg-gray-800 dark:border-gray-700" key={project.id}>
                                                 <th className="px-3 py-2">{project.id}</th>
                                                 <td className="px-3 py-2"><img src={project.image_path} style={{ width: 60 }} alt="image" /></td>
-                                                <td className="px-3 py-2">{project.name}</td>
+                                                <td className="px-3 py-2">
+                                                    <Link href={route('project.show', project.id)}>
+                                                        {project.name}
+                                                    </Link>
+                                                </td>
                                                 <td className="px-3 py-2">
                                                     <span className={"px-2 py-1 rounded text-white " + PROJECT_STATUS_CLASS_MAP[project.status]}>
                                                         {PROJECT_STATUS_TEXT_MAP[project.status]}
