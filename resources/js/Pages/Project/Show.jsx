27323d0187e171ab9da6e7232084af532319ab47
Kombiah <kombiahrk@yahoo.com>
2024-04-12 12:02:36 +0530

    Updating Show.jsx file to display task of selected project details

--- resources/js/Pages/Project/Show.jsx
+++ resources/js/Pages/Project/Show.jsx
@@ -67,7 +67,16 @@ const Show = ({ auth, project }) => {
                     </div>
                 </div>
             </div>
-
+            
+            <div className="py-12">
+                <div className="max-w-7xl mx-auto sm:px-6 lg:px-8">
+                    <div className="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
+                        <div className="p-6 text-gray-900 dark:text-gray-100">
+                            Table goes here
+                        </div>
+                    </div>
+                </div>
+            </div>
         </AuthenticatedLayout>
     )
 }
