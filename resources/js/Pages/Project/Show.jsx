2e018fa3b613d651b4ef3a865651fd1079824614
Kombiah <kombiahrk@yahoo.com>
2024-04-16 12:57:13 +0530

    Modifying Code to hide the project column in TaskTable Component for Projec show Page

--- resources/js/Pages/Project/Show.jsx
+++ resources/js/Pages/Project/Show.jsx
@@ -73,7 +73,7 @@ const Show = ({ auth, project, tasks, queryParams }) => {
                 <div className="max-w-7xl mx-auto sm:px-6 lg:px-8">
                     <div className="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
                         <div className="p-6 text-gray-900 dark:text-gray-100">
-                            <TasksTable tasks={tasks} queryParams={queryParams}/>
+                            <TasksTable tasks={tasks} queryParams={queryParams} hideProjectColumn={true} />
                         </div>
                     </div>
                 </div>
