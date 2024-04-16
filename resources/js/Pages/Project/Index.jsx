e1e3a1354853195f6f679a511f2fbbbd11b473ec
Kombiah <kombiahrk@yahoo.com>
2024-04-16 16:44:31 +0530

    Add Hover Effect for Add New Link

--- resources/js/Pages/Project/Index.jsx
+++ resources/js/Pages/Project/Index.jsx
@@ -44,7 +44,7 @@ export default function Index({ auth, projects, queryParams = null }) {
             header={
                 <div className="flex items-center justify-between">
                     <h2 className="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">Projects</h2>
-                    <Link className="bg-emerald-500 text-white px-3 py-1 rounded" href={route('project.create')}> Add New</Link>
+                    <Link className="bg-emerald-500 text-white px-3 py-1 rounded hover:bg-emerald-600" href={route('project.create')}> Add New</Link>
                 </div>
             }
         >
