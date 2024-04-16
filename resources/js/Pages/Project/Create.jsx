782f0c4114d5838f1c56115f26faf49700821116
Kombiah <kombiahrk@yahoo.com>
2024-04-16 16:28:22 +0530

    Add the Form Elements in Create.jsx Component of Project Folder

--- resources/js/Pages/Project/Create.jsx
+++ resources/js/Pages/Project/Create.jsx
@@ -1,8 +1,24 @@
+import InputError from '@/Components/InputError'
+import InputLabel from '@/Components/InputLabel'
+import SelectInput from '@/Components/SelectInput'
+import TextAreaInput from '@/Components/TextAreaInput'
+import TextInput from '@/Components/TextInput'
 import Authenticated from '@/Layouts/AuthenticatedLayout'
-import { Head } from '@inertiajs/react'
+import { Head, useForm } from '@inertiajs/react'
 import React from 'react'
 
 const Create = ({ auth }) => {
+    const { data, setData, post, errors, reset } = useForm({
+        image: '',
+        name: '',
+        description: '',
+        status: '',
+        due_date: ''
+    })
+    const onSubmit = (e) => {
+        e.preventDefault();
+        post(route('project.create'));
+    };
     return (
         <Authenticated
             user={auth.user}
@@ -15,9 +31,86 @@ const Create = ({ auth }) => {
             <div className="py-12">
                 <div className="max-w-9xl mx-auto sm:px-6 lg:px-8">
                     <div className="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
-                        <div className="p-6 text-gray-900 dark:text-gray-100">
-                            Form
-                        </div>
+                        <form onSubmit={onSubmit} className="p-4 sm:p-8 bg-white dark:bg-gray-800 shadow sm:rounded-lg">
+                            <div>
+                                <InputLabel
+                                    htmlFor="project_image_path"
+                                    value="Project Image"
+                                />
+                                <TextInput
+                                    id="project_image_path"
+                                    type="file"
+                                    name="image"
+                                    value={data.image}
+                                    className="mt-1 block w-full"
+                                    onChange={(e) => setData('image', e.target.value)}
+                                />
+                                <InputError message={errors.image} className="mt-2" />
+                            </div>
+                            <div className='mt-4'>
+                                <InputLabel
+                                    htmlFor="project_name"
+                                    value="Project Name"
+                                />
+                                <TextInput
+                                    id="project_name"
+                                    type="text"
+                                    name="name"
+                                    value={data.name}
+                                    className="mt-1 block w-full"
+                                    onChange={(e) => setData('name', e.target.value)}
+                                />
+                                <InputError message={errors.name} className="mt-2" />
+                            </div>
+                            <div className='mt-4'>
+                                <InputLabel
+                                    htmlFor="project_description"
+                                    value="Project Description"
+                                />
+                                <TextAreaInput
+                                    id="project_description"
+                                    name="description"
+                                    value={data.description}
+                                    className="mt-1 block w-full"
+                                    onChange={(e) => setData('description', e.target.value)}
+                                />
+                                <InputError message={errors.description} className="mt-2" />
+                            </div>
+                            <div className='mt-4'>
+                                <InputLabel
+                                    htmlFor="project_due_date"
+                                    value="Project Deadline"
+                                />
+                                <TextInput
+                                    id="project_due_date"
+                                    type="date"
+                                    name="due_date"
+                                    value={data.due_date}
+                                    className="mt-1 block w-full"
+                                    onChange={(e) => setData('due_date', e.target.value)}
+                                />
+                                <InputError message={errors.due_date} className="mt-2" />
+                            </div>
+                            <div className='mt-4'>
+                                <InputLabel
+                                    htmlFor="project_status"
+                                    value="Project Status"
+                                />
+                                <SelectInput
+                                    id="project_status"
+                                    name="status"
+                                    value={data.status}
+                                    className="mt-1 block w-full"
+                                    onChange={(e) => setData('status', e.target.value)}
+                                >
+                                    <option value="">Select Status</option>
+                                    <option value="pending">Pending</option>
+                                    <option value="in_progress">In Progress</option>
+                                    <option value="completed">Completed</option>
+                                </SelectInput>
+                                <InputError message={errors.status} className="mt-2" />
+                            </div>
+                        </form>
                     </div>
                 </div>
             </div>
