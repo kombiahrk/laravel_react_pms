672a672fd2b8a290e5ad9b263977416f618d4779
Kombiah <kombiahrk@yahoo.com>
2024-04-16 20:04:26 +0530

    create Edit Component for Project by copy paste Create Component of Project and do some modifications

---
+++ resources/js/Pages/Project/Edit.jsx
@@ -0,0 +1,139 @@
+import InputError from '@/Components/InputError'
+import InputLabel from '@/Components/InputLabel'
+import SelectInput from '@/Components/SelectInput'
+import TextAreaInput from '@/Components/TextAreaInput'
+import TextInput from '@/Components/TextInput'
+import Authenticated from '@/Layouts/AuthenticatedLayout'
+import { Head, Link, useForm } from '@inertiajs/react'
+import React from 'react'
+
+const Edit = ({ auth, project }) => {
+    const { data, setData, post, errors, reset } = useForm({
+        image: '',
+        name: project.name || '',
+        description: project.description || '',
+        status: project.status || '',
+        due_date: project.due_date || ''
+    })
+    const onSubmit = (e) => {
+        e.preventDefault();
+        post(route('project.store'));
+    };
+    return (
+        <Authenticated
+            user={auth.user}
+            header={
+                <h2 className="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">
+                    Edit Project : "{project.name}"
+                </h2>
+            }
+        >
+            <Head title="Projects" />
+
+            <div className="py-12">
+                <div className="max-w-9xl mx-auto sm:px-6 lg:px-8">
+                    <div className="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
+                        <form onSubmit={onSubmit} className="p-4 sm:p-8 bg-white dark:bg-gray-800 shadow sm:rounded-lg">
+                            {project.image_path &&
+                                <div className='mb-4'>
+                                    <img src={project.image_path} alt={project.name} className='w-full h-64' />
+                                </div>
+
+                            }
+                            <div>
+                                <InputLabel
+                                    htmlFor="project_image_path"
+                                    value="Project Image"
+                                />
+                                <TextInput
+                                    id="project_image_path"
+                                    type="file"
+                                    name="image"
+                                    className="mt-1 block w-full"
+                                    onChange={(e) => setData('image', e.target.files[0])}
+                                />
+                                <InputError message={errors.image} className="mt-2" />
+                            </div>
+                            <div className='mt-4'>
+                                <InputLabel
+                                    htmlFor="project_name"
+                                    value="Project Name"
+                                />
+                                <TextInput
+                                    id="project_name"
+                                    type="text"
+                                    name="name"
+                                    value={data.name}
+                                    className="mt-1 block w-full"
+                                    onChange={(e) => setData('name', e.target.value)}
+                                />
+                                <InputError message={errors.name} className="mt-2" />
+                            </div>
+                            <div className='mt-4'>
+                                <InputLabel
+                                    htmlFor="project_description"
+                                    value="Project Description"
+                                />
+                                <TextAreaInput
+                                    id="project_description"
+                                    name="description"
+                                    value={data.description}
+                                    className="mt-1 block w-full"
+                                    onChange={(e) => setData('description', e.target.value)}
+                                />
+                                <InputError message={errors.description} className="mt-2" />
+                            </div>
+                            <div className='mt-4'>
+                                <InputLabel
+                                    htmlFor="project_due_date"
+                                    value="Project Deadline"
+                                />
+                                <TextInput
+                                    id="project_due_date"
+                                    type="date"
+                                    name="due_date"
+                                    value={data.due_date}
+                                    className="mt-1 block w-full"
+                                    onChange={(e) => setData('due_date', e.target.value)}
+                                />
+                                <InputError message={errors.due_date} className="mt-2" />
+                            </div>
+                            <div className='mt-4'>
+                                <InputLabel
+                                    htmlFor="project_status"
+                                    value="Project Status"
+                                />
+                                <SelectInput
+                                    id="project_status"
+                                    name="status"
+                                    value={data.status}
+                                    className="mt-1 block w-full"
+                                    onChange={(e) => setData('status', e.target.value)}
+                                >
+                                    <option value="">Select Status</option>
+                                    <option value="pending">Pending</option>
+                                    <option value="in_progress">In Progress</option>
+                                    <option value="completed">Completed</option>
+                                </SelectInput>
+                                <InputError message={errors.status} className="mt-2" />
+                            </div>
+                            <div className='mt-4 text-right'>
+                                <Link
+                                    href={route("project.index")}
+                                    className='bg-gray-100 py-1 px-3 text-gray-800 rounded hover:bg-gray-300 mr-2'
+                                >
+                                    Cancel
+                                </Link>
+                                <button className='bg-emerald-500 text-white px-3 py-1 rounded hover:bg-emerald-600'>
+                                    Submit
+                                </button>
+                            </div>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </Authenticated>
+    )
+}
+
+export default Edit
