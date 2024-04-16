41292c1ed18c7b607be71a003be9770f0bba531a
Kombiah <kombiahrk@yahoo.com>
2024-04-16 13:21:03 +0530

    Content width increased from 7xl to 9xl

--- resources/js/Pages/Project/Show.jsx
+++ resources/js/Pages/Project/Show.jsx
@@ -70,7 +70,7 @@ const Show = ({ auth, project, tasks, queryParams }) => {
             </div>
 
             <div className="py-12">
-                <div className="max-w-7xl mx-auto sm:px-6 lg:px-8">
+                <div className="max-w-9xl mx-auto sm:px-6 lg:px-8">
                     <div className="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
                         <div className="p-6 text-gray-900 dark:text-gray-100">
                             <TasksTable tasks={tasks} queryParams={queryParams} hideProjectColumn={true} />
--- resources/js/Pages/Task/Index.jsx
+++ resources/js/Pages/Task/Index.jsx
@@ -11,7 +11,7 @@ export default function Index({ auth, tasks, queryParams = null }) {
             <Head title="Tasks" />
 
             <div className="py-12">
-                <div className="max-w-7xl mx-auto sm:px-6 lg:px-8">
+                <div className="max-w-9xl mx-auto sm:px-6 lg:px-8">
                     <div className="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
                         <div className="p-6 text-gray-900 dark:text-gray-100">
                             <TasksTable tasks={tasks} queryParams={queryParams} />
