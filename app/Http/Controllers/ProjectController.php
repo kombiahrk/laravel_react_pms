a23924883ba08cd5badeb1be8f5199ada00df985
Kombiah <kombiahrk@yahoo.com>
2024-04-16 19:57:05 +0530

    ProjectController edit() updated to display edit page

--- app/Http/Controllers/ProjectController.php
+++ app/Http/Controllers/ProjectController.php
@@ -99,7 +99,9 @@ class ProjectController extends Controller
      */
     public function edit(Project $project)
     {
-        //
+        return inertia("Project/Edit", [
+            "project" => new ProjectResource($project),
+        ]);
     }
 
     /**
