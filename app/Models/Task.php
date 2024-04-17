4a6e116128c77f95270033a046813472adb4a179
Kombiah <kombiahrk@yahoo.com>
2024-04-17 18:23:02 +0530

    In Task model, fillable property added

--- app/Models/Task.php
+++ app/Models/Task.php
@@ -10,6 +10,19 @@ class Task extends Model
 {
     use HasFactory;
 
+    protected $fillable = [
+        'name',
+        'description',
+        'image_path',
+        'due_date',
+        'status',
+        'priority',
+        'assigned_user_id',
+        'created_by',
+        'updated_by',
+        'project_id'
+    ];
+
     public function project(): BelongsTo
     {
         return $this->belongsTo(Project::class);
