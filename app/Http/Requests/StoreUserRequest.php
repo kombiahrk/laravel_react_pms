3a8543eb1cedfe19d0d73fe99f432fe95a12f73d
Kombiah <kombiahrk@yahoo.com>
2024-04-17 15:43:12 +0530

    StoreUserRequest updated for unique email for users table

--- app/Http/Requests/StoreUserRequest.php
+++ app/Http/Requests/StoreUserRequest.php
@@ -23,8 +23,8 @@ class StoreUserRequest extends FormRequest
     public function rules(): array
     {
         return [
-            'name'  => ['required', 'string', 'max:255'],
-            'email' => ['required', 'email', 'max:255', 'unique:users'],
+            'name' => ['required', 'string', 'max:255'],
+            'email' => ['required', 'email', 'max:255', 'unique:users,email'],
             'password' => [
                 'required',
                 'confirmed',
