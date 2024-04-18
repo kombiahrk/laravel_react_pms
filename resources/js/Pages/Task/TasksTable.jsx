dc996cec57468c0ab72eed5dff38198a93c165cb
Kombiah <kombiahrk@yahoo.com>
2024-04-18 15:08:31 +0530

    Update the Task/TasksTable to display the link on task name to show task details

--- resources/js/Pages/Task/TasksTable.jsx
+++ resources/js/Pages/Task/TasksTable.jsx
@@ -155,7 +155,11 @@ const TasksTable = ({ tasks, queryParams = null, hideProjectColumn = false, succ
                                 {!hideProjectColumn && (
                                     <td className="px-3 py-2 text-nowrap">{task.project.name}</td>
                                 )}
-                                <td className="px-3 py-2 text-nowrap">{task.name}</td>
+                                <td className="px-3 py-2 hover:underline hover:text-white text-nowrap">
+                                    <Link href={route('task.show', task.id)}>
+                                        {task.name}
+                                    </Link>
+                                </td>
                                 <td className="px-3 py-2 text-nowrap">
                                     <span className={"px-2 py-1 rounded text-white " + TASK_STATUS_CLASS_MAP[task.status]}>
                                         {TASK_STATUS_TEXT_MAP[task.status]}
