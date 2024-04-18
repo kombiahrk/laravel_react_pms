25b3896b4c7192dc3d5cf8f45e2c9e86170318e7
Kombiah <kombiahrk@yahoo.com>
2024-04-18 16:19:59 +0530

    DashboardController, index() updated to pass the counts to dasboard widgets

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -2,9 +2,34 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Task;
 use Illuminate\Http\Request;
 
 class DashboardController extends Controller
 {
-    //
+    public function index()
+    {
+        $user = auth()->user();
+
+        $totalPendingTasks = Task::query()->where('status', 'pending')->count();
+
+        $myPendingTasks = Task::query()->where('status', 'pending')->where('assigned_user_id', $user->id)->count();
+
+        $totalInProgressTasks = Task::query()->where('status', 'in_progress')->count();
+
+        $myInProgressTasks = Task::query()->where('status', 'in_progress')->where('assigned_user_id', $user->id)->count();
+
+        $totalCompletedTasks = Task::query()->where('status', 'completed')->count();
+
+        $myCompletedTasks = Task::query()->where('status', 'completed')->where('assigned_user_id', $user->id)->count();
+
+        return Inertia('Dashboard', compact(
+            'totalInProgressTasks',
+            'myInProgressTasks',
+            'totalCompletedTasks',
+            'myCompletedTasks',
+            'totalPendingTasks',
+            'myPendingTasks'
+        ));
+    }
 }
